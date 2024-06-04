56927128149621485991f0e8488e3a3055e3567a
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-04 19:31:28 +0530

    switch

--- test.go
+++ test.go
@@ -1,28 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-
-	if 7%2 == 0 {
-		fmt.Println("7 is even");
-	} else {
-		fmt.Println("7 is odd");
+	i := 2;
+	fmt.Println("Write ", i, " as ");
+	switch i {
+	case 1:
+		fmt.Println("one");
+	case 2: 
+		fmt.Println("two");
+	case 3:
+		fmt.Println("three");
 	}
 
-	if 8 % 4 == 0 {
-		fmt.Println("8 is divisible by 4");
-	} 
+	fmt.Println("Time", time.Sunday);
 
-	if 8 % 2 == 0 || 7 % 2 == 0 {
-		fmt.Println("either 8 or 7 are even");
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
 	}
 
-	if num := 9; num < 0 {
-		fmt.Println("num is negative");
-	} else if num < 10 {
-		fmt.Println(num, "num has one digit");
-	} else {
-		fmt.Println(num, "has multiple digits");
+	t := time.Now();
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It' after noon")
 	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int");
+		default:
+			fmt.Printf("Don't know the type %T\n", t)
+		}
+	}
+	whatAmI(true);
+	whatAmI(1);
+	whatAmI("Hey");
 }
