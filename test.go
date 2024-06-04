4f8c44ab82bb7eedef3e58fe97456a8bce3c0cbb
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-04 22:41:49 +0530

    go-switch

--- test.go
+++ test.go
@@ -7,9 +7,14 @@ import (
 // this is a comment
 
 func main() {
-	fmt.Println(true && true);
-	fmt.Println(true && false);
-	fmt.Println(true || true);
-	fmt.Println(true || false);
-	fmt.Println(!true);
+	for i := 1; i <= 10; i++ {
+		switch i {
+		case 1: fmt.Println("One");
+		case 2: fmt.Println("Two");
+		case 3: fmt.Println("Three");
+		case 4: fmt.Println("Four");
+		case 5: fmt.Println("Five");
+		default: fmt.Println("Unknown");
+		}
+	}
 }
