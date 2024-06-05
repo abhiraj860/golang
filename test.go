640108e7e249f4a4a17ae714e31c95da20085267
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-05 19:39:15 +0530

    add array

--- test.go
+++ test.go
@@ -1,20 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
-// this is a comment
+import "fmt"
 
 func main() {
-	for i := 1; i <= 10; i++ {
-		switch i {
-		case 1: fmt.Println("One");
-		case 2: fmt.Println("Two");
-		case 3: fmt.Println("Three");
-		case 4: fmt.Println("Four");
-		case 5: fmt.Println("Five");
-		default: fmt.Println("Unknown");
-		}
-	}
+	var x[5]int;
+	x[4] = 100;
+	fmt.Println(x[0]);
 }
