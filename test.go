0b338de0f366bce871f93b4aa5876d1cbdaaa5f3
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-05 22:39:49 +0530

    closure

--- test.go
+++ test.go
@@ -2,11 +2,27 @@ package main
 
 import "fmt"
 
+var x int = 5
+
+func f2() (int, int) {
+	return 5, 6
+}
+
+
 func main() {
-	elements := make(map[string]string);
-	elements["H"] = "Hydrogen"
-	elements["He"] = "Helium"
-	elements["Li"] = "Lithium"
-	fmt.Println(elements["Li"])
-	fmt.Println(elements["Un"]);
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
+	fmt.Println(increment())
+}
+
+func average(xs [] float64) float64 {
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
 }
