28e6f2ee7f04b0705de253223e5a3b13b4e268b8
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-06 22:43:58 +0530

    changed in go

--- test.go
+++ test.go
@@ -5,7 +5,7 @@ import "fmt"
 var x int = 5
 
 func f2() (int, int) {
-	return 5, 6
+	return 5, 5
 }
 
 
@@ -13,7 +13,7 @@ func main() {
 	add := func(x, y int) int {
 		return x + y
 	}
-	fmt.Println(add(1, 2))
+	fmt.Println(add(0, 2))
 }
 
 func average(xs [] float64) float64 {
