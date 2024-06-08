290b80e76aba8973010a9b651cccb4c8bce2e32f
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-08 20:18:22 +0530

    circle-are

--- test.go
+++ test.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func one(xPtr *int) {
-	*xPtr = 1
+type Circle struct {
+	x, y, r float64
+}
+
+func (c * Circle) area() float64 {
+	return math.Pi * c.r * c.r
 }
 
 func main() {
-	xPtr := new(int)
-	one(xPtr)
-	fmt.Println(*xPtr)
+	c := &Circle{0, 0, 5};
+	fmt.Println(c.area())
 }
