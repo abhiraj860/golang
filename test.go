b174ae4f79f2a58580b2cebb7088c5c1a49c15a2
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-06-10 09:03:09 +0530

    basic-template

--- test.go
+++ test.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"math"
 )
 
-type Circle struct {
-	x, y, r float64
-}
 
-func (c * Circle) area() float64 {
-	return math.Pi * c.r * c.r
-}
+
 
 func main() {
-	c := &Circle{0, 0, 5};
-	fmt.Println(c.area())
+
 }
